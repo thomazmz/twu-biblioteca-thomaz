8c8ec8f8af03990e25ae430c95307d9d36281642
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-01-27 02:35:18 -0300

    Remove unused code

--- src/main/java/com/twu/biblioteca/domain/book/BookRepository.java
+++ src/main/java/com/twu/biblioteca/domain/book/BookRepository.java
@@ -2,33 +2,12 @@ package com.twu.biblioteca.domain.book;
 
 import com.twu.biblioteca.domain.LoanableRepository;
 
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class BookRepository extends LoanableRepository<Book> {
-
-    public BookRepository() {
-        super();
-    }
 
     public BookRepository(List<Book> books) {
         super();
         books.forEach(book -> create(book));
-    }
-
-    public Set<Book> getAvailableBooks() {
-        return getAll()
-                .stream()
-                .filter(book -> book.isAvailable())
-                .collect(Collectors.toCollection(LinkedHashSet::new));
-    }
-
-    public Set<Book> getUnAvailableBooks() {
-        return getAll()
-                .stream()
-                .filter(book -> !book.isAvailable())
-                .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 }
--- src/main/java/com/twu/biblioteca/domain/movie/MovieRepository.java
+++ src/main/java/com/twu/biblioteca/domain/movie/MovieRepository.java
@@ -5,11 +5,7 @@ import com.twu.biblioteca.domain.LoanableRepository;
 import java.util.List;
 
 public class MovieRepository extends LoanableRepository<Movie> {
-
-    public MovieRepository() {
-        super();
-    }
-
+    
     public MovieRepository(List<Movie> books) {
         super();
         books.forEach(book -> create(book));
