23742f82cce8873773436e3b97e3d67faed5a813
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-02-06 19:35:04 -0300

    Remove string to static field

--- src/main/java/com/twu/biblioteca/domain/user/WrongCredentialsException.java
+++ src/main/java/com/twu/biblioteca/domain/user/WrongCredentialsException.java
@@ -2,7 +2,9 @@ package com.twu.biblioteca.domain.user;
 
 public class WrongCredentialsException extends Exception {
 
+    public static final String WRONG_CREDENTIALS_MESSAGE = "Wrong credentials.";
+
     public WrongCredentialsException() {
-        super("Wrong credentials.");
+        super(WRONG_CREDENTIALS_MESSAGE);
     }
 }
